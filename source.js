7a9d9bab5bd9727b4e4ccc0fe50c62f16617560e
Gabriele-Tedesco <154592056+Gabriele-Tedesco@users.noreply.github.com>
2024-02-17 19:41:32 +0100

    Add files via upload

---
+++ source.js
@@ -0,0 +1,135 @@
+const source = [
+    {
+        "type": "image",
+        "icon": "ico-neve",
+        "title": "gremlins",
+        "url": "images/gremlins.gif"
+    },
+    {
+        "type": "text",
+        "icon": "ico-albero",
+        "text": "Se Babbo Natale mi ha insegnato qualcosa è che, se ti presenti solo una volta l’anno, tutti sono felici di vederti."
+
+    },
+    {
+        "type": "text",
+        "icon": "ico-fiocco",
+        "text": "Se fossi nato fra il 1647 al 1660 non avresti potuto festeggiare il Natale! In quegli anni fu bandito e cambiato in un giorno di digiuno."
+    },
+    {
+        "type": "image",
+        "icon": "ico-stella",
+        "title": "home alone",
+        "url": "images/homealone.gif"
+    },
+    {
+        "type": "text",
+        "icon": "ico-omino",
+        "text": "La stella di Natale sembra una pianta tipicamente invernale, invece nasce come cespuglio nel caldo del Messico ed era conosciuta fin dai tempi degli Aztechi"
+    },
+    {
+        "type": "image",
+        "icon": "ico-stella",
+        "title": "Griffin",
+        "url": "images/griffin.gif"
+    },
+    {
+        "type": "video2",
+        "icon": "ico-pupazzo",
+        "title": "Regali Andrea Galeazzi"
+    },
+    {
+        "type": "text",
+        "icon": "ico-caramella",
+        "text": "Jingle Bells, Jingle Bells: non mentire, l’hai letta cantando. Sapevi che la canzone più famosa del natale è anche stata la prima cantata nello spazio?"
+    },
+    {
+        "type": "image",
+        "icon": "ico-calza",
+        "title": "Skeleton",
+        "url": "images/skeleton.gif"
+    },
+    {
+        "type": "text",
+        "icon": "ico-pupazzo",
+        "text": "Natale a Luglio? A Cuba è stato così per quasi 30 anni, fra il 1968 e il 1997: i festeggiamenti avvenivano in piena estate"
+    },
+    {
+        "type": "text",
+        "icon": "ico-fiocco",
+        "text": "Oltre a Pasqua, anche Natale ha la sua isola: è Christmas Island nell’Oceano Indiano, scoperta il 25 dicembre 1643."
+    },
+    {
+        "type": "text",
+        "icon": "ico-fiocco",
+        "text": "Anche Babbo Natale cede al lato oscuro della Forza. Il suo alter ego è Krampus, un demone che va alla ricerca di bambini cattivi."
+    },
+    {
+        "type": "music",
+        "icon": "ico-calza",
+        "title": "Jingle bell Rock"
+    },
+    {
+        "type": "video",
+        "icon": "ico-stella",
+        "title": "Geopop video",
+    },
+    {
+        "type": "text",
+        "icon": "ico-stella",
+        "text": "In Svezia c’è una versione local di Babbo Natale: si chiama Tomte, è basso come un folletto e gira accompagnato da Yule, la sua capra"
+    },
+    {
+        "type": "video1",
+        "icon": "ico-calza",
+        "title": "Video space valley",
+    },
+    {
+        "type": "text",
+        "icon": "ico-caramella",
+        "text": "<h1> Er Presepio: </h1> <h3><em>(Trilussa)</em></h3> <br> Ve ringrazio de core, brava gente, <br> pé ‘sti presepi che me preparate <br> ma che li fate a fa? Si poi v’odiate, <br> si de st’amore non capite gnente… <br> Pé st’amore sò nato e ce sò morto, <br> da secoli lo spargo dalla croce, <br> ma la parola mia pare ‘na voce <br> sperduta ner deserto, senza ascolto. <br> La gente fa er presepe e nun me sente; <br> cerca sempre de fallo più sfarzoso, <br> però cià er core freddo e indifferente <br> e nun capisce che senza l’amore <br> è cianfrusaja che nun cià valore."
+    },
+    {
+        "type": "text",
+        "icon": "ico-omino",
+        "text": "In Giappone il Natale è festeggiato come una sorta di San Valentino durante il quale solo le coppie si scambiano i doni"
+    },
+    {
+        "type": "text",
+        "icon": "ico-caramella",
+        "text": "È tempo di panettone, ma da dove viene questo nome? È quello di un dolce, il Pan di Toni inventato dall’aiuto cuoco della famiglia Sforza"
+    },
+    {
+        "type": "text",
+        "icon": "ico-calza",
+        "text": "Ma Babbo Natale abita in Puglia? Il nostro barbuto personaggio si ispira a San Nicola di Myra, un vescovo le cui spoglie sono custodite a Bari"
+    },
+    {
+        "type": "image",
+        "icon": "ico-caramella",
+        "title": "Una poltrona per due",
+        "url": "images/poltrona.gif"
+    },
+    {
+        "type": "Video3",
+        "icon": "ico-caramella",
+        "title": "Ricette natalizie con Benedetta Rossi",
+    },
+    {
+        "type": "image",
+        "icon": "ico-neve",
+        "title": "Elf",
+        "url": "images/elf.gif"
+    },
+    {
+        "type": "text",
+        "icon": "ico-neve",
+        "text": "Se pensi di non aver fritto abbastanza durante le scorse festività, ti sproniamo dicendo che in Polonia il pranzo natalizio è di 12 portate, una per ogni apostolo "
+    },
+    {
+        "type": "image",
+        "icon": "ico-caramella",
+        "title": "Carlton dancing",
+        "url": "images/christmas.gif"
+    }
+];
