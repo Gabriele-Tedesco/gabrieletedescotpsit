d1bf20d23c0e3729b7200f5337fee0e4caa8badb
Gabriele-Tedesco <154592056+Gabriele-Tedesco@users.noreply.github.com>
2025-12-29 16:32:20 +0100

    Update xmas-counter.js

--- xmas-counter.js
+++ xmas-counter.js
@@ -5,7 +5,7 @@ const MinutesElm = document.querySelector ('#minutes');
 const SecondsElm = document.querySelector ('#seconds');
 const PanelElm = document.querySelector ('.panel');
 // Qui abbiamo specificato la data
-const EndDate = new Date("december 25 2026");
+const EndDate = new Date("2026-12-25");
 const EndDateInMs = EndDate.getTime();
 
 // Qua abbiamo dei calcoli per trasformare i millisecondi in giorni, ore, minuti e secondi
@@ -32,4 +32,5 @@ function Timer () {
     PanelElm.innerHTML = "<h1>Buon Natale!</h1>"
   }
 }
+
 
